fc18f6215346f3a8957a9e9745353fdeabe53c6a
thomas.pinella@looker.com <thomas.pinella@looker.com>
2019-11-15 11:20:39 -0800

    made suggestions returned in a promise

--- src/data/dashboards/filters/sagas.js
+++ src/data/dashboards/filters/sagas.js
@@ -1,31 +1,33 @@
 import { filterActions, suggestionsLoaded } from './actions'
 import { put, takeEvery } from 'redux-saga/effects'
 
-
 function* fetchFilterSuggestionsSaga(action) {
-  yield console.log('yo dawg')
-
   const { id, value } = action.payload
-  const suggestions = findSuggestions(value)
+  const suggestions = yield findSuggestions(value)
   yield put(suggestionsLoaded(id, suggestions))
 }
-
 
 function* watchUpdateFilter() {
   yield takeEvery(filterActions.UPDATE_FILTER, fetchFilterSuggestionsSaga)
 }
 
-
-
-
-
-
 export const sagas = [watchUpdateFilter]
-
 
 // utils
 
-const findSuggestions = value => {
+const findSuggestions = async value => {
   const numRandStrings = Math.floor(30 * Math.pow(0.75, value.length))
-  return [...Array(numRandStrings).keys()].map(x => Math.random().toString(36).substring(7))
+
+  const delay = getRandomInt(5, 200)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([...Array(numRandStrings).keys()].map(x => Math.random().toString(36).substring(7)))
+    }, delay)
+  })
+}
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
